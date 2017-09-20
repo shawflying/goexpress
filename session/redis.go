9b1ab1ac37345f84a846cb38b7b8795fba918444
yanxxmh <yanxiaoxiao@mallog.cn>
2017-09-20 23:05:22 +0800

    1. 设计sessionid 2. 多次测试解决bug

--- session/redis.go
+++ session/redis.go
@@ -29,21 +29,43 @@ func createClient() *redis.Client {
 var client = createClient()
 
 //获取缓存信息
-func GetRedisCache(cookie, key string) interface{} {
-	val, err := client.Get(namespace + "session:" + cookie).Result()
+func GetRedisCache(SessionId, key string) interface{} {
+	val, err := client.Get(namespace + "session:" + SessionId).Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 	var s map[string]interface{}
 	json.Unmarshal([]byte(val), &s)
-	fmt.Println(comutil.TransInterfaceToString(s))
+	fmt.Println("获取session值：", comutil.TransInterfaceToString(s))
 	fmt.Println("直接获取值：", s[key])
 	return s[key]
 }
 
-//设置缓存
-func SetRedisCache(key string, value map[string]interface{}) error {
-	str := comutil.TransInterfaceToString(value)
-	err := client.Set(namespace+"session:"+key, str, 10000*time.Second).Err()
+//设置缓存 直接存储会重写掉
+func SetRedisCache(cookie string, key string, value interface{}) error {
+	var s map[string]interface{}
+	val, err := client.Get(namespace + "session:" + cookie).Result()
+	if err != nil {
+		s = make(map[string]interface{})
+		s[key] = value
+	} else {
+		json.Unmarshal([]byte(val), &s)
+	}
+
+	fmt.Println("存放session前：", comutil.TransInterfaceToString(s))
+	s[key] = value
+	fmt.Println("存放session后：", comutil.TransInterfaceToString(s))
+	str := comutil.TransInterfaceToString(s)
+	err = client.Set(namespace+"session:"+cookie, str, 10000*time.Second).Err()
 	return err
+}
+
+var SessionId = ""
+
+func GetSession(key string) interface{} {
+	return GetRedisCache(SessionId, key)
+}
+
+func SetSession(key string, value interface{}) error {
+	return SetRedisCache(SessionId, key, value)
 }
--- session/redis_test.go
+++ session/redis_test.go
@@ -6,25 +6,37 @@ import (
 	"testing"
 )
 
-//func TestGetRedisCache(t *testing.T) {
+func TestGetRedisCache(t *testing.T) {
+	cookie := "15806111230"
+	data := GetRedisCache(cookie, "name")
+	fmt.Println(comutil.TransInterfaceToString(data))
+}
+
+//
+//func TestSetRedisCache(t *testing.T) {
 //	cookie := "15806111230"
-//	data := GetRedisCache(cookie, "name")
-//	fmt.Println(comutil.TransInterfaceToString(data))
+//	value := make(map[string]interface{})
+//	value["name"] = "admin123"
+//	value["age"] = 19
+//	value["sex"] = 0
+//	value["address"] = "shanghai"
+//	temp := make(map[string]interface{})
+//	temp["love"] = "ss"
+//	temp["city"] = "nj"
+//	value["data"] = temp
+//
+//	fmt.Println("入参：", comutil.TransInterfaceToString(value))
+//	err := SetRedisCache(cookie, "info", value)
+//	fmt.Println(err)
 //}
 
-func TestSetRedisCache(t *testing.T) {
-	cookie := "15806111230"
-	value := make(map[string]interface{})
-	value["name"] = "admin123"
-	value["age"] = 19
-	value["sex"] = 0
-	value["address"] = "shanghai"
-	temp := make(map[string]interface{})
-	temp["love"] = "ss"
-	temp["city"] = "nj"
-	value["data"] = temp
+func TestGetSession(t *testing.T) {
+	SessionId = "17721021494"
+	GetSession("abc")
+}
 
-	fmt.Println(comutil.TransInterfaceToString(value))
-	err := SetRedisCache(cookie, value)
-	fmt.Println(err)
+func TestSetSession(t *testing.T) {
+	SessionId = "17721021494"
+	SetSession("name", "root")
+	SetSession("openid", "12346666666666666666666")
 }
