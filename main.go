f3d51432776ca663fe8dcefb2bf5de7d14b9af94
yanxxit <yanxxit@163.com>
2017-09-22 10:18:36 +0800

    中间件 处理

--- main.go
+++ main.go
@@ -16,8 +16,19 @@ func (cb *customHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("customHandler!!"))
 }
 
-func main() {
+//中间件
+func middlewaresHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("-------------ok")
+		next.ServeHTTP(w, r)
+	})
+}
 
+//返回结果
+func final(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+func main() {
 	http.Handle("/img/", http.FileServer(http.Dir("static")))
 	http.Handle("/js/", http.FileServer(http.Dir("static")))
 	http.Handle("/css/", http.FileServer(http.Dir("static")))
@@ -26,8 +37,10 @@ func main() {
 	http.HandleFunc("/encode", controllers.Encode)     //code
 	http.HandleFunc("/getInfo", controllers.GetInfo)   //code
 	http.HandleFunc("/postInfo", controllers.PostInfo) //code
+	http.HandleFunc("/p/index", controllers.Home)      //进入首页
 
-	http.HandleFunc("/p/index", controllers.Home) //进入首页
+	HomeHandle := http.HandlerFunc(final) //中间处理
+	http.Handle("/mid", middlewaresHandler(HomeHandle))
 
 	fmt.Println("http://127.0.0.1:8600/")
 	err := http.ListenAndServe(":8600", nil) //&customHandler{}
